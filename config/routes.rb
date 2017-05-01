4a63faa9d86e29f4603027b5c579771dcc391f99
Brad Hudson <bradley.hudson@careerbuilder.com>
2017-04-30 20:42:24 -0400

    users sign up working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get  '/signup',  to: 'users#new'
   root 'welcome#index'
+  get '/', to: 'welcome#index'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  #resources :users
 end
