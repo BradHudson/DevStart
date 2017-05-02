b2a16c317b458e38b179df4d01223b1fa3cfd810
Brad Hudson <bradley.hudson@careerbuilder.com>
2017-05-01 23:33:41 -0400

    successful signups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/', to: 'welcome#index'
+  get '/success', to: 'welcome#success'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get '/users', to: 'users#list'
