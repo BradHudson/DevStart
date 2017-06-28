1d751cf8f4337595a9c54da00ee459ae65bb8776
Brad Hudson <bradrayhudson@gmail.com>
2017-06-28 12:37:49 -0400

    lots of data

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'ckeditor'
 gem 'devise'
 gem 'omniauth-yahoo'
+gem 'httparty'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,9 +4,10 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:yahoo]
 
+
+
   def self.new_with_session(params, session)
     super.tap do |user|
-      binding.pry
       if data = session["devise.yahoo_data"] && session["devise.yahoo_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
@@ -14,9 +15,7 @@ class Admin < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      binding.pry
       # user.email = auth.info.email
       # user.password = Devise.friendly_token[0,20]
       # user.name = auth.info.name   # assuming the user model has a name
@@ -25,5 +24,45 @@ class Admin < ApplicationRecord
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
     end
+
+    @token = auth["credentials"]["token"]
+
+    binding.pry
+    week_number = 1
+    team = 4
+    scoreboard_url = "https://fantasysports.yahooapis.com/fantasy/v2/league/359.l.101698/scoreboard;week=#{ week_number }"
+    players_url =  "https://fantasysports.yahooapis.com/fantasy/v2/team/359.l.101698.t.#{ team }/players"
+
+    # query = {
+    #     "method"     => "neworder",
+    #     "nonce"      => 1404996028,
+    #     "order_type" => "buy",
+    #     "quantity"   => 1,
+    #     "rate"       => 1
+    # }
+    headers = {
+        "Authorization"  => "Bearer #{ @token }",
+        "sign" => "0a3888ac7f8e411ad73a0a503c55db70a291bfb9f9a47147d5200882674f717f6ede475669f3453"
+    }
+
+    response =HTTParty.get(
+        "https://fantasysports.yahooapis.com/fantasy/v2/team/359.l.101698.t.#{ team }/players",
+        :headers => headers)
+
+    File.open("data/rosters/2016/2016_roster_#{ team }.json", 'w') do |f|
+      f.puts response.to_json
+    end
+
+    # while team < 11 do
+    #   response =HTTParty.get(
+    #       "https://fantasysports.yahooapis.com/fantasy/v2/team/359.l.101698.t.#{ team }/players",
+    #       :headers => headers)
+    #
+    #   File.open("data/rosters/2016/2016_roster_#{ team }.json", 'w') do |f|
+    #     f.puts response.to_json
+    #   end
+    #   team += 1
+    # end
+
   end
 end
